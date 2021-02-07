386d69806ce6fe7fe05b2926f212b0b926ff5103
Kangqi Fu <kfu9@wisc.edu>
2021-02-07 04:05:40 -0600

    task1.cpp test pass

--- hw2/task1.cpp
+++ hw2/task1.cpp
@@ -2,18 +2,17 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <>
-// I searched how to do timing from https://www.geeksforgeeks.org/measure-execution-time-function-cpp/
-
+#include <chrono>
+// I searched how to do timing from timing.md
 using namespace std;
-using namespace std::chrono;
+using namespace chrono;
 
 int main(int argc, char *argv[]){
 	size_t n = atol(argv[1]);
 	float *arr = (float*)malloc(n * sizeof(float));
 	float *output = (float*)malloc(n * sizeof(float));
 	
-	for (int t = 0; i < n; i++) {
+	for (size_t i = 0; i < n; i++) {
 		arr[i] = 2 * ((float) rand()) / RAND_MAX - 1;
 	}
 	
@@ -21,10 +20,23 @@ int main(int argc, char *argv[]){
 	scan(arr, output, n);
 	auto stop = high_resolution_clock::now();
 	
-	auto duration = duration_cast<milliseconds>(stop - start);
-	cout << "Time taken by scan function: " << duration.count() << " milliseconds" << endl;
+	//auto duration = duration_cast<milliseconds>(stop - start);
+	auto duration_sec = duration_cast<duration<double, std::milli>>(stop - start);
+	cout << "Time taken by scan function: " << duration_sec.count() << " milliseconds" << endl;
 	cout << "First element in output is " << output[0] << endl;
 	cout << "Last element in output is " << output[n-1] << endl;
+	
+	// some test functions
+//	for (size_t i=0; i < n; i++) {
+//		cout << arr[i] << ' ';
+//	}
+//	
+//	cout << endl;
+//	for (size_t i=0; i < n; i++) {
+//		cout << output[i] << ' ';
+//	}
+//	cout << endl;
+	
 	free(arr);
 	free(output);
 }
