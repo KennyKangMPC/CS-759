67402c7638e27cbd12d5afa0c555b44bf010dc2b
Kangqi Fu <kfu9@wisc.edu>
2021-02-07 03:28:55 -0600

    update task1.cpp

--- hw2/task1.cpp
+++ hw2/task1.cpp
@@ -1,7 +1,26 @@
 #include "scan.h"
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <>
+// I searched how to do timing from https://www.geeksforgeeks.org/measure-execution-time-function-cpp/
 
 using namespace std;
+using namespace std::chrono;
+
 int main(int argc, char *argv[]){
+	size_t n = atol(argv[1]);
+	float *arr = (float*)malloc(n * sizeof(float));
+	float *output = (float*)malloc(n * sizeof(float));
 	
+	for (int t = 0; i < n; i++) {
+		arr[i] = 2 * ((float) rand()) / RAND_MAX - 1;
+	}
+	
+	auto start = high_resolution_clock::now();
+	
+	auto stop = high_resolution_clock::now();
+	
+	free(arr);
+	free(output);
 }
