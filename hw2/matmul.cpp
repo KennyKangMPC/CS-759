f764323d6c90206b68a7532747b6c00842a77b42
Kangqi Fu <kfu9@wisc.edu>
2021-02-11 04:42:24 -0600

    update matmul.cpp

---
+++ hw2/matmul.cpp
@@ -0,0 +1,20 @@
+#include "matmul.h"
+
+void mmul1(const double* A, const double* B, double* C, const unsigned int n) {
+	
+}
+
+
+void mmul2(const double* A, const double* B, double* C, const unsigned int n) {
+		
+}
+
+
+void mmul3(const double* A, const double* B, double* C, const unsigned int n) {
+		
+}
+
+
+void mmul4(const std::vector<double>& A, const std::vector<double>& B, double* C, const unsigned int n) {
+	
+}
