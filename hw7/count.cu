42befd4126cd60adafebaf73e2add52616dd877f
Kangqi Fu <kfu9@wisc.edu>
2021-03-18 07:16:31 -0500

    finish count.cu

---
+++ hw7/count.cu
@@ -0,0 +1,16 @@
+#include "count.cuh"
+#include <thrust/device_vector.h>
+
+void count(const thrust::device_vector<int>& d_in,
+                 thrust::device_vector<int>& values,
+                 thrust::device_vector<int>& counts) {
+	
+	thrust::device_vector<int> dVal(int(d_in.size()));
+    thrust::fill(dVal.begin(), dVal.end(), 1);
+	values = d_in;
+	thrust::sort(values.begin(), values.end());
+	thrust::sort(values.begin(), values.end());
+	auto back = thrust::reduce_by_key(values.begin(), values.end(), dVal.begin(), values.begin(), counts.begin());
+	values.resize(back.first - values.begin());
+	counts.resize(back.second - counts.begin());
+}
