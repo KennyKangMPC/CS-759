d63f3a8a3a784f83e7d70a33b2601cf62aabc6ab
Kangqi Fu <kfu9@wisc.edu>
2021-03-24 07:50:43 -0500

    remove useless files for final project

--- final_project/examples/serial_code/two_level_qg/integer_types.h
+++
@@ -1,6 +0,0 @@
-! Integer precision types: single byte, double bytes, quad bytes, oct bytes.
-! E.g., sb can hold up to 10^2, db can hold up to 10^4.
-INTEGER, PARAMETER :: sb = SELECTED_INT_KIND(2)
-INTEGER, PARAMETER :: db = SELECTED_INT_KIND(4)
-INTEGER, PARAMETER :: qb = SELECTED_INT_KIND(8)
-INTEGER, PARAMETER :: ob = SELECTED_INT_KIND(16)
