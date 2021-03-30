2ad99223617bbc95adfd15c6c41b8f1202709bb6
Kangqi Fu <kfu9@wisc.edu>
2021-03-30 16:45:01 -0500

    upload bash and plot files for task 1

---
+++ hw9/task1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#SBATCH -p wacc
+#SBATCH -J task1
+#SBATCH -o %x.out -e %x.err
+#SBATCH -N 1 -c 20
+#SBATCH -t 0-00:30:00
+
+g++ task1.cpp cluster.cpp -Wall -O3 -o task1 -fopenmp
+
+for t in {1..10}; do
+  echo "Running with t = $t"
+  ./task1 5040000 $t
+  echo
+done
