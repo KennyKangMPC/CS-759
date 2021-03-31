85e7a03a55feaa6b735ca84fdb5a6e191d37b972
Kangqi Fu <kfu9@wisc.edu>
2021-03-31 17:11:56 -0500

    upload bash files for task 2

---
+++ hw9/task2_wo_simd.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#SBATCH -p wacc
+#SBATCH -J task2_wo_simd
+#SBATCH -o %x.out -e %x.err
+#SBATCH -N 1 -c 20
+#SBATCH -t 0-00:30:00
+
+module load gcc/10.2.0
+
+g++ task2.cpp montecarlo.cpp -Wall -O3 -std=c++17 -o task2 -fopenmp -fno-tree-vectorize -march=native -fopt-info-vec
+
+for t in {1..10}; do
+  echo "Running with t = $t"
+  ./task2 1000000 $t
+  echo
+done
