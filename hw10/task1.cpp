c9a2adb4ed1d34f118de85d0bdc0003d2cbf83d8
Kangqi Fu <kfu9@wisc.edu>
2021-04-06 13:53:02 -0500

    finish task1.cpp : NEED TEST

---
+++ hw10/task1.cpp
@@ -0,0 +1,111 @@
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <stdio.h>
+#include "optimize.h"
+
+using namespace std;
+using namespace chrono;
+
+double time1(vec *v, data_t *dest) {
+	auto start = high_resolution_clock::now();
+	optimize1(v, dest);
+	auto end = high_resolution_clock::now();
+	double duration = duration_cast<duration<double, std::milli>>(end - start).count();
+	return duration;
+}
+
+double time2(vec *v, data_t *dest) {
+	auto start = high_resolution_clock::now();
+	optimize2(v, dest);
+	auto end = high_resolution_clock::now();
+	double duration = duration_cast<duration<double, std::milli>>(end - start).count();
+	return duration;
+}
+
+double time3(vec *v, data_t *dest) {
+	auto start = high_resolution_clock::now();
+	optimize3(v, dest);
+	auto end = high_resolution_clock::now();
+	double duration = duration_cast<duration<double, std::milli>>(end - start).count();
+	return duration;
+}
+
+double time4(vec *v, data_t *dest) {
+	auto start = high_resolution_clock::now();
+	optimize4(v, dest);
+	auto end = high_resolution_clock::now();
+	double duration = duration_cast<duration<double, std::milli>>(end - start).count();
+	return duration;
+}
+
+double time5(vec *v, data_t *dest) {
+	auto start = high_resolution_clock::now();
+	optimize5(v, dest);
+	auto end = high_resolution_clock::now();
+	double duration = duration_cast<duration<double, std::milli>>(end - start).count();
+	return duration;
+}
+
+int main(int argc, char *argv[]) {
+	int n = atol(argv[1]);
+	data_t *data = new data_t[n];
+	vec v = vec(n);
+	
+	for (int i = 0; i < n; i++) {
+		// to make things simple, I choose to fill all 1
+		data[i] = 1;
+	}
+	v.data = data;
+	data_t dest;
+	
+	int iterations = 10;
+	
+	// next is for optimize 1
+	double time = 0;
+	for (int i = 0; i < iterations; i++) {
+		time += time1(&v, &dest);
+	}
+    time /= num_iterations;
+    cout << dest << endl;
+    cout << time << endl;
+    
+    // next is for optimize 2
+	time = 0;
+	for (int i = 0; i < iterations; i++) {
+		time += time2(&v, &dest);
+	}
+    time /= num_iterations;
+    cout << dest << endl;
+    cout << time << endl;
+    
+    // next is for optimize 3
+	time = 0;
+	for (int i = 0; i < iterations; i++) {
+		time += time3(&v, &dest);
+	}
+    time /= num_iterations;
+    cout << dest << endl;
+    cout << time << endl;
+    
+    // next is for optimize 4
+	time = 0;
+	for (int i = 0; i < iterations; i++) {
+		time += time4(&v, &dest);
+	}
+    time /= num_iterations;
+    cout << dest << endl;
+    cout << time << endl;
+    
+    // next is for optimize 5
+	time = 0;
+	for (int i = 0; i < iterations; i++) {
+		time += time5(&v, &dest);
+	}
+    time /= num_iterations;
+    cout << dest << endl;
+    cout << time << endl;
+}
+
+
